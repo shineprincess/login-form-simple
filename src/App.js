01e1831bba6aa51ee81a7c08679b1eba01fec114
shineprincess <shineprincess456@gmail.com>
2021-02-11 21:09:58 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [allEntry , setAllEntry] = useState([]);
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    const newEntry = {email:email , password:password };
+    setAllEntry([...allEntry, newEntry]);
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+    <div  className= "main_div">
+      <form onSubmit={submitForm} className="form">
+        <div className="login__form">
+          <label htmlFor="email"> Email</label>
+          <input
+          className="input"
+            type="text"
+            name="email"
+            id="email"
+            autoComplete="off"
+            value = {email}
+            onChange={(event) =>  setEmail(event.target.value)}
+          ></input>
+        </div>
+
+        <div className="login__form">
+          <label htmlFor="password"> Password </label>
+          <input
+          className="input"
+            type="password"
+            name="password"
+            id="password"
+            autoComplete="off"
+            value = {password}
+            onChange ={(event) =>  setPassword(event.target.value)}
+          ></input>
+        </div>
+
+        <button  className = "btn_submit" type="submit"> Login </button>
+      </form>
+
+      <div className="showData__style">
+        {
+          allEntry.map((currElem) => {
+            return(
+              <div className="showdata">
+                <p className="show__email">  {currElem.email} </p>
+                <p className="show__password"> {currElem.password} </p>
+              </div>
+            );
+          })
+        }
+      </div>
+      </div>
+
+    </>
   );
-}
+};
 
 export default App;
