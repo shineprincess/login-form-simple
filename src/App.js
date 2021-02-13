68869cb5ee36436323abb7240e14878610fdd1f6
Priyanka dhongde <69955610+shineprincess@users.noreply.github.com>
2021-02-13 03:06:11 -0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import MultipleInputs from "./Components/MultipleInputs";
+
+const App = () => {
+  return (
+    <>
+      <MultipleInputs />
+    </>
+  );
+};
+
+export default App;
